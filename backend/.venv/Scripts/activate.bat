3baaa9380164160cf2a0411268ba6ab9772055c6
anthony-2903 <73666364+anthony-2903@users.noreply.github.com>
2026-07-02 16:53:52 +0200

    .

--- backend/.venv/Scripts/activate.bat
+++ backend/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\Alumno Cisco\Documents\GitHub\tesis-lstm-gru\backend\.venv"
+set "VIRTUAL_ENV=C:\Users\AnthonyJc\Documents\GitHub\tesis-lstm-gru\backend\.venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
